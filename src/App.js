fa0b5ff8e68ecc8a900df6a9c4b60932406e0cf7
yoosion030 <rsy5487@naver.com>
2022-10-05 19:26:24 +0900

    ⚡️ :: 라우트 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import { Route, Routes } from "react-router-dom";
-
+import { Route, Routes } from 'react-router-dom';
+import * as P from './pages';
 function App() {
   return (
     <Routes>
-      <Route />
+      <Route path="/" element={<P.Main />} />
+      <Route path="/setting" element={<P.Setting />} />
+      <Route path="/connect" element={<P.Connect />} />
+      <Route path="/user" element={<P.User />} />
+      <Route path="/community" element={<P.Community />} />
+      <Route path="/login" element={<P.Login />} />
     </Routes>
   );
 }
