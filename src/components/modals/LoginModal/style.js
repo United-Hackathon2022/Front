df2362f35bda76aa1caa67c5a5feaa8bc9bb16bb
yoosion030 <rsy5487@naver.com>
2022-10-06 13:17:07 +0900

    📑 :: 로그인 모달 추가

---
+++ src/components/modals/LoginModal/style.js
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const Button = styled.button`
+  width: 363px;
+  height: 54px;
+  background: #fafafa;
+  border: 1px solid #f0f0f0;
+  border-radius: 6px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  cursor: pointer;
+`;
