9bad2f3d5ee77d14440395f1e53d09268acf09c5
yoosion030 <rsy5487@naver.com>
2022-10-06 19:28:35 +0900

    url 수정

--- src/components/modals/LoginModal/index.js
+++ src/components/modals/LoginModal/index.js
@@ -2,12 +2,11 @@ import ModalContainer from '../../ModalContainer/Modals';
 import LoginImg from '../../../assets/modal_img/login.svg';
 import GoogleIcon from '../../../assets/icon/naver.svg';
 import * as S from './style';
-import auth from '../../../api/auth';
 
 const LoginModal = () => {
   const TryLogin = async () => {
     window.location.href =
-      'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=uZW5eGAiHkbEdvpSzfxn&redirect_uri=https://hackathon.dsm-pick.com/&state=hackathon';
+      'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=uZW5eGAiHkbEdvpSzfxn&redirect_uri=http://localhost:3000/&state=hackathon';
   };
   return (
     <ModalContainer
