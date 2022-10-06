83284dc4c8cb20e737fe0f8710d4a912d7063ba6
yoosion030 <rsy5487@naver.com>
2022-10-06 20:46:04 +0900

    ⚡️ :: Write 컴포넌트 스타일 추가

--- src/components/Write/Write.jsx
+++ src/components/Write/Write.jsx
@@ -1,10 +1,17 @@
 import * as S from './Write.style';
+import MOON_LOGO from '../../assets/logo_img/moon.svg';
 
 const Write = () => {
   return (
     <S.Body>
       <S.WriteSection>
-        <div />
+        <S.TitleInput placeholder="제목을 입력하세요." />
+        <S.ImgSection>
+          <hr />
+          <img src={MOON_LOGO} alt="" />
+        </S.ImgSection>
+        <S.ContentInput placeholder="당신의 이야기를 적어보세요..." />
+        <S.Button>등록하기</S.Button>
       </S.WriteSection>
     </S.Body>
   );
--- src/components/Write/Write.style.js
+++ src/components/Write/Write.style.js
@@ -9,8 +9,67 @@ export const Body = styled.div`
 `;
 
 export const WriteSection = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 100%;
   height: 1000px;
-
+  padding: 50px;
   background-color: #ffffff;
+
+  textarea {
+    border: none;
+    width: 100%;
+
+    &:focus {
+      outline: none;
+    }
+  }
+`;
+
+export const TitleInput = styled.textarea`
+  height: 40px;
+  font-size: 28px;
+  color: #515151;
+  font-family: 'TmoneyRoundWindExtraBold';
+  resize: none;
+`;
+
+export const ImgSection = styled.div`
+  display: flex;
+  align-items: flex-end;
+  gap: 10px;
+  margin-bottom: 10px;
+
+  hr {
+    margin: 0;
+    display: inline-block;
+    width: 70px;
+    height: 5px;
+    background: #ffcf4f;
+    border: none;
+  }
+  img {
+    width: 18px;
+  }
+`;
+
+export const ContentInput = styled.textarea`
+  font-size: 14px;
+  color: #8e8e8e;
+  resize: none;
+  flex: 1;
+`;
+
+export const Button = styled.button`
+  width: 478px;
+  height: 48px;
+  background: #ffcf4f;
+  border-radius: 6px;
+  border: none;
+  font-size: 21px;
+  line-height: 27px;
+  color: #ffffff;
+  text-align: center;
+
+  cursor: pointer;
 `;
