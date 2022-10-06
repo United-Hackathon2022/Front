7e8061f607b10d5e5a61577c46b135a006eec207
yoosion030 <rsy5487@naver.com>
2022-10-06 16:51:04 +0900

    📑 :: Util 생성

---
+++ src/Utils/Libs/requestApi.ts
@@ -0,0 +1,30 @@
+import { baseURL } from '../../shared/baseURL';
+import axios, { AxiosRequestConfig } from 'axios';
+
+const RequestApi = (p: AxiosRequestConfig, token?: string) => {
+  try {
+    const res = token
+      ? axios({
+          method: p.method,
+          baseURL: baseURL,
+          url: p.url,
+          data: p.data,
+          withCredentials: true,
+          headers: {
+            cookie: token,
+          },
+        })
+      : axios({
+          method: p.method,
+          baseURL: baseURL,
+          url: p.url,
+          data: p.data,
+          withCredentials: true,
+        });
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
+export default RequestApi;
